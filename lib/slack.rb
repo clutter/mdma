c14088874f26980deaf0f897b186036501687ea5
Robert Manson <robmanson@gmail.com>
2017-12-15 14:20:52 -0800

    Put Slack Token in Credentials

--- lib/slack.rb
+++ lib/slack.rb
@@ -2,7 +2,7 @@ require 'rest-client'
 
 class Slack
   def self.notify(message)
-    RestClient.post "https://hooks.slack.com/services/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", {'text' => message}.to_json, {content_type: :json, accept: :json}
+    RestClient.post Rails.application.credentials.dig(:slack, :token_url), {'text' => message}.to_json, {content_type: :json, accept: :json}
   end
 end
 
