8393678c757fd83db55fa0dca005a4e0ab84c820
Claudio Baccigalupo <32649767+claudioclutter@users.noreply.github.com>
2018-02-09 10:58:53 -0800

    Fix Layout/SpaceInsideHashLiteralBraces

--- lib/slack.rb
+++ lib/slack.rb
@@ -2,6 +2,6 @@ require 'rest-client'
 
 class Slack
   def self.notify(message)
-    RestClient.post Rails.application.credentials.dig(:slack, :token_url), {'text' => message}.to_json, {content_type: :json, accept: :json}
+    RestClient.post Rails.application.credentials.dig(:slack, :token_url), { 'text' => message }.to_json, content_type: :json, accept: :json
   end
 end
