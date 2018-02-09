5edad394838d482b6ebad6df6d9c574c5a22fe24
Claudio Baccigalupo <32649767+claudioclutter@users.noreply.github.com>
2018-02-09 11:14:47 -0800

    Fix Metrics/LineLength

--- lib/slack.rb
+++ lib/slack.rb
@@ -2,6 +2,7 @@ require 'rest-client'
 
 class Slack
   def self.notify(message)
-    RestClient.post Rails.application.credentials.dig(:slack, :token_url), { 'text' => message }.to_json, content_type: :json, accept: :json
+    credentials = Rails.application.credentials.dig(:slack, :token_url)
+    RestClient.post credentials, { 'text' => message }.to_json, content_type: :json, accept: :json
   end
 end
