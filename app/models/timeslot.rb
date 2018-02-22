b2bbd64416cf19c70d803a7068fbb44e3580f2f0
Claudio Baccigalupo <32649767+claudioclutter@users.noreply.github.com>
2018-02-22 14:13:50 -0800

    Always have at least one timeslot

--- app/models/timeslot.rb
+++ app/models/timeslot.rb
@@ -2,4 +2,13 @@
 class Timeslot < ActiveRecord::Base
   has_many :deploys, dependent: :destroy
   scope :enabled, -> { where(enabled: true) }
+
+  before_validation :set_default_values, on: :create
+
+private
+
+  def set_default_values
+    self.delay_in_hours ||= 0
+    self.prefixes ||= ['']
+  end
 end
