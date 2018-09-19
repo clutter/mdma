ee97aafe820f8071877f7f33e282c453f889587a
Claudio Baccigalupo <32649767+claudioclutter@users.noreply.github.com>
2018-09-18 17:55:44 -0700

    Fix test: add required timeslot

--- spec/system/timeslots_spec.rb
+++ spec/system/timeslots_spec.rb
@@ -2,6 +2,8 @@ RSpec.describe 'The timeslots page', :logged_in do
   before { visit timeslots_url }
 
   context 'given some timeslots' do
+    before(:all) { Timeslot.create! }
+
     it 'displays prefixes and delays of each enabled timeslot' do
       expect(page).to have_css 'tbody tr'
     end
