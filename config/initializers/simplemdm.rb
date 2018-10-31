f9999486ee0f77c6a66666fdfe441beb87c9b918
Claudio Baccigalupo <32649767+claudioclutter@users.noreply.github.com>
2018-10-31 16:02:07 -0700

    Support pagination in Device.all
    According to the [SimpleMDM API documentation](https://simplemdm.com/docs/api/#pagination):
    
    > Pagination functionality became available on August 15, 2018.
    > Accounts created before this date have until November 15, 2018
    > to update their integrations to support pagination.
    > On November 15, 2018, pagination will become forced.

--- config/initializers/simplemdm.rb
+++ config/initializers/simplemdm.rb
@@ -1,11 +1,29 @@
 # Extend SimpleMDM Ruby API to support refresh
 module SimpleMDM
-  # Adds the refresh method to SimpleMDM::Device
   class Device < Base
+    # Adds the refresh method to SimpleMDM::Device
     def refresh
       raise "You cannot refresh device that hasn't been created yet." if new?
       hash, code = fetch("devices/#{id}/refresh", :post)
       code == 202
+    end
+
+    # Overwrites the fetch method to go through each page.
+    def self.all
+      devices = []
+      starting_after = nil
+      more_pages = true
+
+      while more_pages
+        params = { limit: 100, starting_after: starting_after }.compact
+        resp = RestClient.get "#{SimpleMDM.api_url}devices", params: params
+        hash = JSON.parse(resp)
+        devices += hash['data'].collect { |d| build d }
+        starting_after = hash['data'].last['id']
+        more_pages = hash['has_more']
+      end
+
+      devices
     end
   end
 end
