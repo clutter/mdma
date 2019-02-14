510b35fd574bc8a48bde40996712466e70b4d9dc
Claudio Baccigalupo <32649767+claudioclutter@users.noreply.github.com>
2019-02-14 14:36:22 -0800

    Compact the PackageAnalyzer in a single file
    It's useful to split long files into separate files/classes.
    However, in the case of this analyzer, it might be useful to
    keep everything in a single place, so it's easier to reason about.

--- app/analyzers/package_analyzer.rb
+++ app/analyzers/package_analyzer.rb
@@ -1,26 +1,46 @@
 require 'zip'
 require 'zip/filesystem'
+require 'cfpropertylist'
 
+# Extracts the following from an iOS App Store Package (.ipa):
+#
+# * Display Name
+# * Identifier
+# * Short Version
+# * Version
+# * Minimum iOS version
+#
+# Example:
+#
+#   PackageAnalyzer.new(blob).metadata
+#   # => {:bundle_display_name=>"Clutter", :bundle_identifier=>"com.clutter.app",
+#         :bundle_short_version=>"1.0", :bundle_version=>"42", :bundle_minimum_os_version=>"11.0"}
 class PackageAnalyzer < ActiveStorage::Analyzer
   def metadata
-    read_info_plist do |info_plist|
-      return {} unless info_plist
-
-      {
-        bundle_display_name: info_plist.bundle_display_name,
-        bundle_identifier: info_plist.bundle_identifier,
-        bundle_short_version: info_plist.bundle_short_version,
-        bundle_version: info_plist.bundle_version,
-        bundle_minimum_os_version: info_plist.bundle_minimum_os_version,
-      }.compact
+    download_blob_to_tempfile do |file|
+      Zip::File.open(file.path) do |package|
+        plist_data = extract_plist_data_from package
+        plist_dict = CFPropertyList::List.new(data: plist_data).value
+        plist_hash = CFPropertyList.native_types plist_dict
+        relevant_keys.transform_values { |identifier| plist_hash[identifier] }.compact
+      end
     end
   end
 
 private
 
-  def read_info_plist
-    download_blob_to_tempfile do |file|
-      yield IPA.new(file.path).info_plist
-    end
+  def extract_plist_data_from(package)
+    app_name = package.dir.entries('Payload').find { |entry_name| entry_name =~ /.app$/ }
+    package.get_entry("Payload/#{app_name}/Info.plist").get_input_stream.read
+  end
+
+  def relevant_keys
+    {
+      bundle_display_name: 'CFBundleDisplayName',
+      bundle_identifier: 'CFBundleIdentifier',
+      bundle_short_version: 'CFBundleShortVersionString',
+      bundle_version: 'CFBundleVersion',
+      bundle_minimum_os_version: 'MinimumOSVersion'
+    }
   end
 end
