1ce8dc2070db02595faec3764a515a6ec7a6bc5b
Claudio Baccigalupo <32649767+claudioclutter@users.noreply.github.com>
2019-09-13 10:04:13 -0700

    Support multiple channels for Slack notifications

--- lib/slack.rb
+++ lib/slack.rb
@@ -2,10 +2,10 @@ require 'rest-client'
 
 # Slack.notify sends a notification to Slack about deploys
 class Slack
-  def self.notify(message)
+  def self.notify(message, channel: ENV['SLACK_CHANNEL'])
     credentials = Rails.application.credentials.dig(:slack, :token_url)
     payload = { 'text' => message }
-    payload['channel'] = ENV['SLACK_CHANNEL'] if ENV['SLACK_CHANNEL'].present?
+    payload['channel'] = channel if channel.present?
     RestClient.post credentials, payload.to_json, content_type: :json, accept: :json
   end
 end
