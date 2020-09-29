27409475eccff51b9158fc40a90d9473e24c9912
Gilbert Fu <gilbert.fu@clutter.com>
2020-09-29 13:24:46 -0700

    Add ability to set minimum required version for build

---
+++ app/controllers/builds/minimum_supported_versions_controller.rb
@@ -0,0 +1,12 @@
+module Builds
+  # Controller to handle setting the build that is the minimum supported version.
+  class MinimumSupportedVersionsController < ApplicationController
+    before_action :require_authentication
+
+    # /builds/:id/minimum_supported_version
+    def update
+      Build.find(params[:build_id]).update!(minimum_supported_version: true)
+      redirect_back fallback_location: root_path, notice: 'New minimum supported version.'
+    end
+  end
+end
